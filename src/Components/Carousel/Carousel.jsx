05a792df8c0bad52f49ba5510e8094c34e6abf21
Marosh Sykora <sykoramaros@gmail.com>
2025-04-28 13:10:14 +0200

    Carousel slider button map upgraded

--- src/Components/Carousel/Carousel.jsx
+++ src/Components/Carousel/Carousel.jsx
@@ -2,6 +2,8 @@ import React from "react"
 import "./Carousel.css"
 
 const Carousel = ({ picture }) => {
+  const slider = picture
+
   return (
     <>
       <div className="carousel-container">
@@ -48,26 +50,17 @@ const Carousel = ({ picture }) => {
             ></span>
           </button>
           <div className="carousel-indicators">
-            <button
-              type="button"
-              data-bs-target="#carouselExampleAutoplaying"
-              data-bs-slide-to="0"
-              className="active"
-              aria-current="true"
-              aria-label="Slide 1"
-            ></button>
-            <button
-              type="button"
-              data-bs-target="#carouselExampleAutoplaying"
-              data-bs-slide-to="1"
-              aria-label="Slide 2"
-            ></button>
-            <button
-              type="button"
-              data-bs-target="#carouselExampleAutoplaying"
-              data-bs-slide-to="2"
-              aria-label="Slide 3"
-            ></button>
+            {slider.map((sld, index) => (
+              <button
+                key={index}
+                type="button"
+                data-bs-target="#carouselExampleAutoplaying"
+                data-bs-slide-to={index}
+                className={index === 0 ? "active" : ""}
+                aria-current={index === 0 ? "true" : undefined}
+                aria-label={`Slide ${index + 1}`}
+              ></button>
+            ))}
           </div>
         </div>
       </div>
--- src/Pages/Cogeneration/Cogeneration.jsx
+++ src/Pages/Cogeneration/Cogeneration.jsx
@@ -10,17 +10,65 @@ const Cogeneration = () => {
   const linked_tertiary_heading = "Projekce"
 
   const pictures = [
+    // {
+    //   src: `${process.env.PUBLIC_URL}/img/main_vyroba-a-pronajem-mobilnich-kotelen.png`,
+    //   text: "Výroba a pronájem mobilních kotelen",
+    // },
+    // {
+    //   src: `${process.env.PUBLIC_URL}/img/TND_kotelna-2-x-3t-pary-scaled.jpg`,
+    //   text: "TND kotelna",
+    // },
+    // {
+    //   src: `${process.env.PUBLIC_URL}/img/realizace_ref_1.png`,
+    //   text: "Výměník",
+    // },
     {
-      src: `${process.env.PUBLIC_URL}/img/main_vyroba-a-pronajem-mobilnich-kotelen.png`,
-      text: "Výroba a pronájem mobilních kotelen",
+      src: `${process.env.PUBLIC_URL}/img/800_600.png`,
+      text: "800 x 600",
     },
     {
-      src: `${process.env.PUBLIC_URL}/img/TND_kotelna-2-x-3t-pary-scaled.jpg`,
-      text: "TND kotelna",
+      src: `${process.env.PUBLIC_URL}/img/1024_768.png`,
+      text: "1024 x 768",
     },
     {
-      src: `${process.env.PUBLIC_URL}/img/realizace_ref_1.png`,
-      text: "Výměník",
+      src: `${process.env.PUBLIC_URL}/img/1920_1080.png`,
+      text: "1920 x 1080",
+    },
+    {
+      src: `${process.env.PUBLIC_URL}/img/2560_1440.png`,
+      text: "2560 x 1440",
+    },
+    {
+      src: `${process.env.PUBLIC_URL}/img/120_120.png`,
+      text: "120 x 120",
+    },
+    {
+      src: `${process.env.PUBLIC_URL}/img/250_250.png`,
+      text: "250 x 250",
+    },
+    {
+      src: `${process.env.PUBLIC_URL}/img/800_600w.png`,
+      text: "800 x 600w",
+    },
+    {
+      src: `${process.env.PUBLIC_URL}/img/1024_768w.png`,
+      text: "1024 x 768w",
+    },
+    {
+      src: `${process.env.PUBLIC_URL}/img/1920_1080w.png`,
+      text: "1920 x 1080w",
+    },
+    {
+      src: `${process.env.PUBLIC_URL}/img/2560_1440w.png`,
+      text: "2560 x 1440w",
+    },
+    {
+      src: `${process.env.PUBLIC_URL}/img/120_120w.png`,
+      text: "120 x 120w",
+    },
+    {
+      src: `${process.env.PUBLIC_URL}/img/250_250w.png`,
+      text: "250 x 250w",
     },
   ]
 
